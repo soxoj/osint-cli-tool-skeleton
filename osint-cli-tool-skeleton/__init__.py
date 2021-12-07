cf8dd7aa89e1c2c9ff1a58bb335b9cf72dcdcc84
Soxoj <soxoj@protonmail.com>
2021-12-07 15:32:13 +0300

    Updates for PyPi publishing

--- osint-cli-tool-skeleton/__init__.py
+++ osint-cli-tool-skeleton/__init__.py
@@ -1,5 +1,7 @@
 from ._version import __version__
 
+def run():
+	print('hello')
 
 if __name__ == '__main__':
 	run()
--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup, find_packages
 
-exec(open('skeleton/_version.py').read())
+exec(open('osint-cli-tool-skeleton/_version.py').read())
 
 with open('requirements.txt') as rf:
     requires = rf.read().splitlines()
 
+with open('README.md') as fh:
+    long_description = fh.read()
+
 setup(
-    name="skeleton",
+    name="osint-cli-tool-skeleton",
     version=__version__,
     description="A skeleton for OSINT CLI tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/soxoj/osint-cli-tool-skeleton",
     author="Soxoj",
     author_email="soxoj@protonmail.com",
-    entry_points={'console_scripts': ['skeleton = skeleton.__init__:run']},
+    entry_points={'console_scripts': ['osint_cli_tool_skeleton = osint_cli_tool_skeleton.__init__:run']},
     license="MIT",
     packages=find_packages(),
     include_package_data=True,
